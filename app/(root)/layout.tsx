9027542a732ca365930421116905b14379fe1056
Mehaksaluja <salujamehak11@gmail.com>
2025-04-29 20:03:31 +0530

    Home Page Completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import Link from "next/link";
 import { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="auth-layout">{children}</div>;
+  return (
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.png" alt="Logo" width={38} height={32} />
+          <h3 className="text-primary-100">InterPrep</h3>
+        </Link>
+      </nav>
+      {children}
+    </div>
+  );
 };
 export default RootLayout;
